b9dc85dd8065c93d177d1e9cd551f8dee08dd703
Yutan0423 <yuutan3240@gmail.com>
2021-01-09 11:04:11 +0900

    課題提出テスト
    1-3フォルダの作成

---
+++ 1-3/main.js
@@ -0,0 +1,29 @@
+let a = 9;
+let b = 14;
+let x = 10;
+let y = 0;
+
+// 問１
+if(a != 10) {
+    console.log(a);
+}
+
+if(b >= 10 && b < 20 && b % 2 == 0) {
+    console.log(b);
+}
+
+
+// 問2
+if(x >= 10 && x <= 20) {
+    console.log("成功です");
+} else {
+    console.log("失敗です");
+}
+
+
+// 問3
+if(y % 2 ==0) {
+    console.log("偶数です");
+} else if(y % 2 != 0) {
+    console.log("奇数です");
+}
